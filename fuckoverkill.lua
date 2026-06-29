e693016713dfb39f4edeadba72ed7076684f7c96
glonkered-ui <glonkered@gmail.com>
2026-06-29 02:33:58 +0200

    Update fuckoverkill.lua

--- fuckoverkill.lua
+++ fuckoverkill.lua
@@ -774,7 +774,7 @@ end)
 
 if UI_Loaded and UI_Library then
     Window = UI_Library:CreateWindow({
-        Title = "fuck overkill - made with ♥ by Glonk (tf3m on discord)",
+        Title = "Glonk's Enhancements - made with ♥ by Glonk (tf3m on discord)",
         Center = true,
         AutoShow = true,
     })
